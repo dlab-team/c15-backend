470438e6a005859dc3e6154fd1cb732db2472677
Felipe <lfazocar@users.noreply.github.com>
2024-01-31 20:57:37 -0300

    feature: .env.example

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
 const database = new Sequelize(process.env.POSTGRES_DATABASE, process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, {
-    host: process.env.POSTGRES_HOST,
+    host: 'postgresdb',
     port: process.env.POSTGRES_DOCKER_PORT,
     dialect: 'postgres'
 });
