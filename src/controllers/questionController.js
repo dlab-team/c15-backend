188d12480c34a5a411bdd72ecbfe896df21d384d
Cinthya <cinthya@mycnote.com>
2024-02-22 22:32:42 -0300

    moving logic from questionRoutes to questionController

--- src/controllers/questionController.js
+++ src/controllers/questionController.js
@@ -1,12 +1,20 @@
 import  models from '../models/index.js'
-const question = models.Question
-export const createQuestion = async (questionText, pillarId) => {
+const questionModel = models.Question
+
+ async function create(req, res, next) {
     try {
-       const newQuestion = await question.create({
-        question: questionText, fk_id_pillar: pillarId
-    });
-      return newQuestion
+        const { question, pillarId } = req.body;
+        if (question && pillarId) {
+           const newQuestion = await questionModel.create({
+            question: question, fk_id_pillar: pillarId
+        });
+        res.status(201).json({  message: `Question ${newQuestion.id} successfully created`  });
+        } else {
+          return res.status(400).json({ success: false, message: "Required data is missing (question, pillarId)" });
+       }
     } catch (error) {
-        throw new Error(error);
+        return next(error);
     }
 }
+
+export default {create}
--- src/routes/questionRoutes.js
+++ src/routes/questionRoutes.js
@@ -1,21 +1,8 @@
 import express from 'express';
 const router = express.Router();
 
-import * as questionController from '../controllers/questionController.js';
+import questionController from '../controllers/questionController.js';
 
-router.post("/create", async (req, res, next) => {
-    try {
-        const { question, pillarId } = req.body;
-        if (question && pillarId) {
-           const newQuestion = await questionController.createQuestion(question, pillarId)
-            res.status(201).json({
-                message: `Question ${newQuestion.id} successfully created`  });
-        } else {
-          return res.status(400).json({ success: false, message: "Required data is missing (question, pillarId)" });
-       }
-    } catch (error) {
-        return next(error);
-    }
-});
+router.post("/create", questionController.create);
 
 export default router;
