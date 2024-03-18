2faf41f0692673af518eb33ca47e68cf0d07a92f
Felipe <lfazocar@users.noreply.github.com>
2024-03-18 20:42:25 -0300

    feat: added roles to token validation middleware

--- src/middleware/tokenValidation.js
+++ src/middleware/tokenValidation.js
@@ -2,25 +2,28 @@ import jwt from "jsonwebtoken";
 import models from "../models/index.js";
 const { User, InvalidToken } = models;
 
-async function validateUserId(req, res, next) {
-    try {
-        const token = req.header("authorization");
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        const invalidToken = await InvalidToken.findOne({
-            where: { token: token },
-            attributes: ['token']
-        });
-        const user = await User.findByPk(req.params.id, { attributes: ['password_date'] });
-        const password_date = Math.floor(user.password_date.getTime()/1000)
-        if (invalidToken || decoded.iat < password_date) {
-            return res.status(401).json({ message: 'Error 401: Unauthorized' });
-        };
-        if (req.params.id != decoded.sub) {
-            return res.status(403).json({ message: 'Error 403: Forbidden' });
-        };
-        next();
-    } catch (error) {
-        return res.status(400).json({ message: error.message });
+function validateUser (roles = [1, 2]) {
+    return async (req, res, next) => {
+        try {
+            const token = req.header("authorization");
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+            const invalidToken = await InvalidToken.findOne({
+                where: { token: token },
+                attributes: ['token']
+            });
+            const user = await User.findByPk(decoded.sub, { attributes: ['password_date', 'role_id'] });
+            const password_date = Math.floor(user.password_date.getTime()/1000)
+            if (invalidToken || decoded.iat < password_date) {
+                return res.status(401).json({ message: 'Error 401: Unauthorized' });
+            };
+            if (req.params.id == decoded.sub || roles.includes(user.role_id)) {
+                next();
+            } else {
+                return res.status(403).json({ message: 'Error 403: Forbidden' });
+            };
+        } catch (error) {
+            return res.status(400).json({ message: error.message });
+        }
     }
 };
 
@@ -40,6 +43,6 @@ async function validateRecovery (req, res, next) {
 };
 
 export {
-    validateUserId,
+    validateUser,
     validateRecovery
 };
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,13 +1,13 @@
 import { Router } from 'express';
 const router = Router();
 import userController from '../controllers/userController.js';
-import { validateUserId } from '../middleware/tokenValidation.js';
+import { validateUser } from '../middleware/tokenValidation.js';
 
-router.get('/', userController.index);
-router.get('/:id', validateUserId, userController.read);
+router.get('/', validateUser([2]), userController.index);
+router.get('/:id', validateUser([2]), userController.read);
 router.post('/', userController.create);
-router.put('/:id', validateUserId, userController.update);
-router.delete('/:id', validateUserId, userController.destroy);
-router.put('/:id/password', validateUserId, userController.change_password);
+router.put('/:id', validateUser([]), userController.update);
+router.delete('/:id', validateUser([2]), userController.destroy);
+router.put('/:id/password', validateUser([]), userController.change_password);
 
 export default router;
